adbf79876a3182b5b47be60d8e8f0134aae506b5
ravi8010 <46920264+ravi8010@users.noreply.github.com>
2019-01-24 16:47:23 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,32 @@
+package com.capgemini.beans;
+
+public class Student {
+	private  int rl;
+	private  String name;
+private	String[] enroll;
+	public void setEnroll(String[] enroll) {
+	this.enroll = enroll;
+}
+	
+
+	public int getRl() {
+		return rl;
+	}
+	public void setRl(int rl) {
+		this.rl = rl;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String[] getEnroll() {
+		return enroll;
+	}
+	
+	
+
+}
